eb46d2b3d443152e1e86c106b67c8109dd083155
Alex Nogueira <alex@devaction.com.br>
2026-05-24 10:21:42 -0300

    fix: add error code 10000 (authentication_error) and bump to 0.1.1

--- lib/swoosh_cloudflare/error.ex
+++ lib/swoosh_cloudflare/error.ex
@@ -8,6 +8,7 @@ defmodule SwooshCloudflare.Error do
 
   def from_response(status, _body), do: {status, :unknown_error}
 
+  defp map_code(10000), do: :authentication_error
   defp map_code(10001), do: :invalid_request
   defp map_code(10200), do: :invalid_content
   defp map_code(10202), do: :message_too_large
